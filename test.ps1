edd49d7ced2334d12f964c9691ecf862582037f1
Timileyin <66250695+oloruntolaallbert@users.noreply.github.com>
2024-01-05 13:22:27 +0000

    Add files via upload

---
+++ test.ps1
@@ -0,0 +1,14 @@
+Invoke-WebRequest "https://raw.githubusercontent.com/olafhartong/sysmon-modular/master/sysmonconfig.xml" -OutFile sysmonlatest.xml -UseBasicParsing
+Invoke-WebRequest "https://github.com/kinnairdt/Scripts/blob/main/mdr-supporting-files-main/sysmon.zip" -OutFile "sysmon.zip" -UseBasicParsing
+Expand-Archive "sysmon.zip" -Force
+
+if ([Environment]::Is64BitOperatingSystem) {
+Copy-Item ".\sysmon64.exe" "C:\windows\system32\sysmon64.exe"
+Start-Process "C:\Windows\system32\Sysmon64.exe" -ArgumentList "-accepteula -i sysmonlatest.xml"
+Start-Process "C:\Windows\system32\Sysmon64.exe" -ArgumentList "-accepteula -c sysmonlatest.xml"
+}
+else {
+Copy-Item ".\sysmon.exe" "C:\windows\system32\sysmon.exe"
+Start-Process "C:\Windows\system32\Sysmon.exe" -ArgumentList "-accepteula -i sysmonlatest.xml"
+Start-Process "C:\Windows\system32\Sysmon.exe" -ArgumentList "-accepteula -c sysmonlatest.xml"
+}
